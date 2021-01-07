4fa0d77175523ae3fdaf5fcaa6b0c98b9ba94c59
Domagoj Teskera <domagoj.teskera030@gmail.com>
2021-01-07 13:59:08 +0100

    server i datatable

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+
 const app = express();
+
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-//middleware
+
+
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
-
 
 app.get('/index', (req, res, next) => {
     res.sendFile(__dirname + '/public/index.html');
@@ -20,5 +23,8 @@ app.get('/datatable', (req, res, next) => {
     res.sendFile(__dirname + '/public/datatable.html');
 });
 
+app.post('/F1timovi.json', (req, res) => {
+    console.log(req.body);
+});
 
 app.listen(PORT);
