8481c38578fdb23a9030de5875f2a31b3885e345
Domagoj Teskera <domagoj.teskera030@gmail.com>
2021-01-18 19:13:24 +0100

    napravljen id endpoint i picture endpoint za svaki id

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const fs = require('fs');
+
 const app = express();
 
 const path = require('path');
@@ -8,10 +10,13 @@ const PORT = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
 
+const request = require('request');
+
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,6 +28,49 @@ app.get('/index', (req, res, next) => {
 
 app.get('/datatable', (req, res, next) => {
     res.sendFile(__dirname + '/public/datatable.html');
+});
+
+app.get('/datatable/:id', (req, res, next) => {
+    let obj = JSON.parse(fs.readFileSync(__dirname + "/public/F1timovi.json", 'utf8'));
+    let pas = null;
+    for (let i = 0; i < obj.length; i++) {
+        let property = obj[i];
+        if(property["_id"]["$oid"] == req.params.id) {
+            pas = property;
+        }
+    }
+    res.render('info', {
+        tim: pas
+    });
+});
+
+app.get('/datatable/:id/picture', async (req, res, next) => {   
+    let obj = JSON.parse(fs.readFileSync(__dirname + "/public/F1timovi.json", 'utf8'));
+    let pas = null;
+    for (let i = 0; i < obj.length; i++) {
+        let property = obj[i];
+        if(property["_id"]["$oid"] == req.params.id) {
+            pas = property;
+        }
+    }
+    let poveznica = pas.poveznica;
+    let link = "https://en.wikipedia.org/api/rest_v1/page/summary/" + poveznica;
+    request(link , { json: true },   async (e, r, body) => {
+        if (e) { return console.log(e); }
+        if(body.originalimage == null) {
+            res.send("wiki api ne radi lmao");
+        } else {
+            let image = body.originalimage.source;
+            if (fs.existsSync("./public/" + poveznica + ".jpg")) {
+                res.sendFile(__dirname + '/public/' + poveznica + '.jpg');               
+            } else {
+                request(image).pipe(fs.createWriteStream("./public/"+poveznica + '.jpg')).on('close', ()=>{
+                    res.sendFile(__dirname + '/public/' + poveznica + '.jpg');
+                });
+            }          
+        }
+      });
+
 });
 
 app.post('/F1timovi.json', (req, res) => {
